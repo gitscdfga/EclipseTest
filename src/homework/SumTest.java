0ab93827ed51cb958d38278423fcc479edc130c5
dell <dell@DESKTOP-VO1MVVV>
2020-02-18 02:51:13 +0800

    2

---
+++ src/homework/SumTest.java
@@ -0,0 +1,21 @@
+package homework;
+
+public class SumTest {
+	public static void sum(int a,int b) {
+		int i;
+		int sum;
+		sum = 0;
+		for(i=a;i<=b;i++) {
+			sum += i;
+		}
+		System.out.println(a+"到"+b+"的和为"+sum);
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sum(10,20);
+		sum(30,40);
+		sum(50,60);
+	}
+
+}
